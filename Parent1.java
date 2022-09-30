a86cc3114a34890b7192e76c7f3f6b947e81986d
anushkakaushik <79746679+anushkakaushik@users.noreply.github.com>
2022-09-30 12:16:23 +0530

    Update Parent1.java

--- Parent1.java
+++ Parent1.java
@@ -1,4 +1,10 @@
 package CoreJava;
+/*
+Program: create a class parent with default constructor and void sing method.print something inside the constructor and method
+Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program.
+@author: Anushka kaushik
+*/
 // multilevel inheritance
  class Parent1 {
 	 // default constructor of class parent1
